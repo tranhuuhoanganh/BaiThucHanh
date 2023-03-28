68943b6a19f00dd00a77aef22bc1efbff0761e8c
tranhuuhoanganh <hoanganhngoc2912@gmail.com>
2023-03-28 07:11:00 +0700

    commit note

--- BaiThucHanhLab2/src/baithuchanhlab2/bai11.java
+++ BaiThucHanhLab2/src/baithuchanhlab2/bai11.java
@@ -15,14 +15,30 @@ public class bai11 {
         
         for( i = 0;i<n;i++)
         {
-            System.out.printf("gia tri thu %d: ",i+1);
+            System.out.printf("arr[%d]: ",i+1);
             arr[i] = sc.nextInt();
         }
+        
+        System.out.println("mang vua nhap la: ");
         for( i = 0;i<n;i++)
         {
             System.out.print(arr[i]+"\t");
         }
           
-        
+        int temp;
+        temp = arr[0];
+        for ( i = 0 ; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        System.out.println("\nsap xep theo thu tu tang dan la: ");
+        for ( i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
     }
 }
